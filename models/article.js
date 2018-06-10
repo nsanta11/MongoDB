279a063ed4d232facf1b251e84fe7c8501206de1
Nicole Santarsiero <santarsiero.nicole@gmail.com>
2018-06-09 17:05:30 -0700

    recent commit

---
+++ models/article.js
@@ -0,0 +1,29 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var articleSchema = new Schema({
+    // title is required for the article
+    title: {
+      type: String,
+      required: true
+    },
+    // article link
+    link: {
+      type: String,
+      required: true
+    },
+    // note: the object that stores the note id
+    note: {
+      type: Schema.Types.ObjectId,
+      ref: "note"
+    }
+  });
+  
+  //model for the schema
+  var article = mongoose.model("article", articleSchema);
+  
+  // Export article model
+  module.exports = article;
+  
+
---
+++ models/index.js
@@ -0,0 +1,6 @@
+// Export object that contains the models
+
+module.exports = {
+    article: require("./article"),
+    note: require("./note")
+  };

9162242aea216ac01da0eef0f9d3ad42d5c2c468
Nicole Santarsiero <santarsiero.nicole@gmail.com>
2018-06-12 20:04:33 -0700

    added promise

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI 
 // || "mongodb://localhost/mongoHeadlines";
+mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
 

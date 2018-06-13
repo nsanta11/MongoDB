0982f9e3467ae0cd45634a386bbbc7ab215e7ac7
Nicole Santarsiero <santarsiero.nicole@gmail.com>
2018-06-12 20:21:04 -0700

    PORT addition

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 // Initialize Express
 var app = express();
